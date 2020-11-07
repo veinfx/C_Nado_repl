3a9c0580470ff6cb58445b11d74323cd7fb8445c
Vein Hub <vein90@naver.com>
2020-11-07 16:21:45 +0000

    나도코딩 repl > git 조건문 시작하기전 테스트

--- if.c
+++ if.c
@@ -1,1 +1,8 @@
 // Add if
+
+#include <stdio.h>
+
+int main(void) {
+  printf("조건문 시작하기전 Git 테스트 \n");
+  return 0;
+}
--- loop.c
+++ loop.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) {
+int main_loop(void) {
 	// printf("Hello World\n");
 	// printf("Hello World\n");
 	// printf("Hello World\n");
