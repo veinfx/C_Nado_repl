a52aa2c0dc503ee5099b995b250d36023670d329
Vein Hub <vein90@naver.com>
2020-11-15 12:54:08 +0000

    [repl] 6-4 문자 vs 문자열

--- array.c
+++ array.c
@@ -50,6 +50,30 @@ int main(void)
         //     printf("%.2f\n", arr_f[i]);
         // }
 
+        // 6-4 문자 vs 문자열
+        // char c = 'A';
+        // printf("%c\n",c);
+
+        // 문자열 긑에는 '끝'을 의미하는 NULL 문자 '\0' 이 포함되어야 함
+        //char str[6] = "coding";// [c][o][d][i][n][g][\0]
+        // char str[7] = "coding";
+        // printf("%s\n",str);
+
+        //char str[]= "coding";
+        // printf("%s\n",str);
+        // printf("%d\n",sizeof(str));
+
+        // for(int i = 0; i <sizeof(str); i++)  //한글자 씩 뽑아서 (출력 문장려 끝 NULL 포함됨) 
+        // {
+        //     printf("%c\n",str[i]);
+        // }
+
+        char kor[] = "나도코딩";
+        printf("%s\n",kor);
+        printf("%d\n",sizeof(kor));
+        //영어는 한글자 : 1바이트, 한글은 2바이트
+        //char 크기 : 1 byte
+
         return 0;
         
 
